fe5af99634eab0ec6328f0556d5ced2a31ecf712
Michael W <mwerbo2@gmail.com>
2019-07-04 23:05:10 -0500

    Clean in routes

--- Routes/announcementRoutes.js
+++ Routes/announcementRoutes.js
@@ -15,10 +15,8 @@ module.exports = app => {
   app.get("/announcements", getAllAnnouncements);
   app.post("/announcements", checkJwt, createAnnouncement);
   app.put("/announcements/:id", checkJwt, updateAnnoucement);
-  // app.post('/announcements', updateOrCreateAnnouncement)
   app.get("/announcements/live", getLiveAnnouncements);
   app.post("/announcements/status", checkJwt, setAnnouncementStatus);
-  // app.get('/announcements/liveStatus', getLiveAnnouncementWithStatus)
   app.get("/announcements/liveStatus", getLiveAnnouncementsRonak);
   app.get("/announcements/status", getAnnouncementByStatus);
 };
