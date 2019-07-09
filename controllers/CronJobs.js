0414cb939c8fe4b73f93034de7bef8f8d1a43e35
Michael W <mwerbo2@gmail.com>
2019-07-08 22:40:40 -0500

    Clean cronjob tbc

--- controllers/CronJobs.js
+++ controllers/CronJobs.js
@@ -1,20 +1,21 @@
-
-const CronJob = require('cron').CronJob;
+const CronJob = require("cron").CronJob;
 import Announcement from "../models/announcementmodel";
 import Schedule from "../models/schedulemodel";
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
-import { getLiveAnnouncementWithStatus, getAllAnnouncements } from './announcementcontroller'
-import { SetInactiveStatus } from './statusUpdateController';
+import {
+  getLiveAnnouncementWithStatus,
+  getAllAnnouncements
+} from "./announcementcontroller";
+import { SetInactiveStatus } from "./statusUpdateController";
 
+console.log("Before job instantiation");
+const job = new CronJob("* * * * * *", function() {
+  // getLiveAnnouncementWithStatus();
 
-console.log('Before job instantiation');
-const job = new CronJob('* * * * * *', function() {
-    // getLiveAnnouncementWithStatus();
-    // getAllAnnouncements();
-    SetInactiveStatus();
+  SetInactiveStatus();
 });
-console.log('After job instantiation');
+console.log("After job instantiation");
 // job.start();
 
 export { job };
