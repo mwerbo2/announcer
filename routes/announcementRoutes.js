e15ded54041418717eee903c9b5f42b7a959078f
Michael Werbowsky <mwerbo2@gmail.com>
2020-03-15 22:02:27 -0500

    Add in tiny mce editor and date time picker to modal

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -7,13 +7,15 @@ import {
   setAnnouncementStatus,
   getLiveAnnouncementWithStatus,
   getLiveAnnouncementsRonak,
-  getAnnouncementByStatus
+  getAnnouncementByStatus,
+  postAndSchedule
 } from "../controllers/announcementcontroller";
 import checkJwt from "../authConfig";
 
 module.exports = app => {
   app.get("/announcements", getAllAnnouncements);
   app.post("/announcements", checkJwt, createAnnouncement);
+  app.post("/announcements/one", checkJwt, postAndSchedule);
   app.put("/announcements/:id", checkJwt, updateAnnoucement);
   app.get("/announcements/live", getLiveAnnouncements);
   app.post("/announcements/status", checkJwt, setAnnouncementStatus);
